2538574722c88438c4caa1d52ebd0efa25618169
Douglas Hennrik <douglashennrik@gmail.com>
2024-09-25 10:44:08 -0300

    Altera a classe Cart para Static

--- src/entities/cart.ts
+++ src/entities/cart.ts
@@ -1,21 +1,22 @@
 import { Product } from "./product";
 
 export class Cart {
-  private _products: Product[] = [];
-  private _total: number = 0;
+  private static _products: Product[] = [];
+  private static _total: number = 0;
 
-  addToCart(product: Product) {
+  static addToCart(product: Product) {
     // Atualiza o valor total da compra
-    this._total += product.price;
+   this._total += product.total;
+
     // Adiciona o produto ao carrinho
     this._products.push(product);
   }
 
-  get products() {
+  static get products() {
     return this._products;
   }
 
-  get total() {
+  static get total() {
     return this._total;
   }
 }
